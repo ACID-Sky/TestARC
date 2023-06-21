bddf429e27249880f5999122d5fae9765d7b48fa
Aleksey Nebesniy <103752883+ACID-Sky@users.noreply.github.com>
2023-06-21 07:20:11 +0300

    Initial Commit

---
+++ Test ARC/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Test ARC
+//
+//  Created by Лёха Небесный on 21.06.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
